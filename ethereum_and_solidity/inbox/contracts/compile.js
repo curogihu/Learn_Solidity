173fb1457cf5ee32eadfd9886203a29e8aacc08f
curogihu <curoas@yahoo.co.jp>
2019-06-16 18:07:43 +0900

    fix bug and add third const and last sentence

--- ethereum_and_solidity/inbox/contracts/compile.js
+++ ethereum_and_solidity/inbox/contracts/compile.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const fs = require('fs');
+const solc = require('solc');
 
-const inboxPath = path.resolve(__dirname, 'Inbox.sol');
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-solc.compile(source, 1);
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
