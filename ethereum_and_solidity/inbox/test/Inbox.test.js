648bee1684e12cbfd3c4841a58450c99c6f34f39
curogihu <curoas@yahoo.co.jp>
2019-06-16 22:08:54 +0900

    refactor getting accounts

--- ethereum_and_solidity/inbox/test/Inbox.test.js
+++ ethereum_and_solidity/inbox/test/Inbox.test.js
@@ -7,24 +7,30 @@ const Web3 = require('web3');
 // v1.x.x Support for promises + async / await
 
 const web3 = new Web3(ganache.provider());
+let accounts;
 
 // Mocha Functions
 // it           - Run a test and make an assertion
 // describe     - Groups together 'it' function
 // beforeEach   - Execute some general setup code
 
-beforeEach(() => {
+beforeEach(async () => {
     // Get a list of all accounts
+/*   
     web3.eth.getAccounts()
         .then(fetchedAccounts => {
             console.log(fetchedAccounts);
         });
+*/
+    accounts = await web3.eth.getAccounts();
 
     // Use one of those accounts to deploy the contract
 
 });
 
 describe('Inbox', () => {
-    it('deploys a contract', () => {})
+    it('deploys a contract', () => {
+      console.log(accounts);  
+    })
 });
 
