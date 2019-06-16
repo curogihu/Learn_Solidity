f75a99ce8f98499cef1259c56419e18e4e73e538
curogihu <curoas@yahoo.co.jp>
2019-06-16 22:33:05 +0900

    Integrate codes

--- ethereum_and_solidity/inbox/test/Inbox.test.js
+++ ethereum_and_solidity/inbox/test/Inbox.test.js
@@ -1,13 +1,17 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
 // Web3 versioning
 // v0.x.x "Primitive" interface - only callbacks for async code
 // v1.x.x Support for promises + async / await
 
-const web3 = new Web3(ganache.provider());
+
+
 let accounts;
+let inbox;
 
 // Mocha Functions
 // it           - Run a test and make an assertion
@@ -25,12 +29,15 @@ beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
 
     // Use one of those accounts to deploy the contract
-
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments: ['Hi there'] })
+        .send({ from: accounts[0], gas: '1000000' })
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-      console.log(accounts);  
+      // console.log(accounts);
+      console.log(inbox);  
     })
 });
 
